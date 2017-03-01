57dd3e60ec18999f8a821ea334413e20b6ad6ac3
Jayden Maddison <jay.maddison@maersk.com>
2017-03-01 23:22:50 +0000

    pig latin files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TranslatorComponent } from './translator/translator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
